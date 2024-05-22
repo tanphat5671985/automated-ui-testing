4f5b50e21aca1cd92d8da783188679f893528baf
Phat <tanphat5671985@gmail.com>
2024-05-22 16:31:15 +0700

    upload code with full homepage automated testing

--- cypress/e2e/homepage.feature
+++ cypress/e2e/homepage.feature
@@ -23,4 +23,59 @@ Background: General Steps
         When Click on a carousel icon below slideshow
         Then A selected product is displayed accurately
 ###TEST CASE CATEGORY
+    Scenario: Verify that categories menu displays correctly on the left page
+        Then The CATEGORIES menu display the left of the page
+        And The list of group product is displayed accurately
+
+    Scenario: Verify that the user can view the specific list of products by clicking a Group Product in a Categories menu
+        Then The CATEGORIES menu display the left of the page
+        When Click on a Group Product on Categories menu
+        Then The product list is changed according to the selected product group
+
+    Scenario: Verify that the user can view the whole list of products by clicking Category menu
+        Then The CATEGORIES menu display the left of the page
+        When Click on a Group Product on Categories menu
+        Then The product list is changed according to the selected product group
+        When Click on CATEGORIES menu field
+        Then The list of product displays correctly with all products
+###TEST CASE PRODUCT CARD
+    Scenario: Verify that the list of product card display correctly
+        Then The list of product displays correctly with all products
     
+    Scenario: Verify that the information of the product card is display correctly
+        Then The card display information correctly
+
+    Scenario: Verify that the title of the product card is underlined when the user hover
+        Then The list of product displays correctly with all products
+        When Hover any title of product card
+        Then The title card is underlined
+
+    Scenario: Verify that the user can redirect to an Information product screen when clicking a photo of the product 
+        When Click on a Group Product on Categories menu
+        Then The product list is changed according to the selected product group
+        When Click on any photo of product card
+        Then Redirect to an Information Product screen
+
+    Scenario: Verify that the user can redirect to an Information product screen when clicking a title of the product 
+        When Click on a Group Product on Categories menu
+        Then The product list is changed according to the selected product group
+        When Click on any title of product card
+        Then Redirect to an Information Product screen
+###TEST CASE PAGINATION
+    Scenario: Verify the user can view next or previous page with whole products by cliking Next/Previous button 
+        Then The list of product displays correctly with all products
+        And The 2 buttons of pagination displays on the right and below list of cards
+        When Click on Next button
+        Then The next page displays with remaining product cards
+        When Click on Previous button
+        Then The previous page displays the list of first product cards
+    
+    # ##product ko đủ để thực hiện case bên dưới
+    # Scenario: Verify the user can view next or previous page with a group products by cliking Next/Previous button 
+    #     Then The list of product displays correctly with all products
+    #     When Click on a Group Product on Categories menu
+    #     And The 2 buttons of pagination displays on the right and below list of cards
+    #     When Click on Next button
+    #     Then The next page displays with remaining product cards
+    #     When Click on Previous button
+    #     Then The previous page displays the list of first product cards
--- cypress/e2e/steps/homepage.steps.js
+++ cypress/e2e/steps/homepage.steps.js
@@ -27,10 +27,10 @@ Then('Slideshow displays a product for 3 seconds and switch to next product',()=
     // Kiểm tra xem slide đầu tiên có class 'active' hay không
     cy.get('.carousel-item').eq(0).should('have.class', 'active');
     // Chờ cho slide thứ 2 xuất hiện
-    cy.wait(6000)
+    cy.wait(5500)
     cy.get('.carousel-item').eq(1).should('have.class', 'active');
     // Chờ cho slide thứ 3 xuất hiện
-    cy.wait(6000)
+    cy.wait(5500)
     cy.get('.carousel-item').eq(2).should('have.class', 'active');
     
 });
@@ -50,11 +50,115 @@ When('Click on a previous icon next to slideshow',()=>{
 Then('A previous product is displayed accurately',()=>{
     cy.get('.carousel-item').eq(2).should('have.class', 'active');
 })
-
+let randomIndex;
 When('Click on a carousel icon below slideshow',()=>{
-    cy.get('.carousel-indicators').find('li').eq(2).click()
+    //cy.get('.carousel-indicators').find('li').eq(2).click()
+    //random
+    randomIndex = Math.floor(Math.random() * 3);
+    cy.log(randomIndex)
+    cy.get('.carousel-indicators').find('li').eq(randomIndex).then($slide => {
+        cy.wrap($slide).click();
+        cy.log(randomIndex)
+    });
 })
 
 Then('A selected product is displayed accurately',()=>{
-    cy.get('.carousel-item').eq(2).should('have.class', 'active');
+    cy.get('.carousel-item').eq(randomIndex).should('have.class', 'active');
+})
+
+Then('The CATEGORIES menu display the left of the page', ()=>{
+    cy.get('#cat').should('exist')
+    cy.get('#cat').should('contain.text','CATEGORIES')
+})
+
+Then('The list of group product is displayed accurately',()=>{
+    cy.get('.list-group-item').should('have.length', 4)
+    
+    cy.get('.list-group-item').eq(1).should('contain.text','Phones')
+    cy.get('.list-group-item').eq(2).should('contain.text','Laptops')
+    cy.get('.list-group-item').eq(3).should('contain.text','Monitors')
+})
+
+let selectedGroupProduct;
+When('Click on a Group Product on Categories menu',()=>{
+    //cy.get('#itemc').eq(0).click()
+    const randomIndex = Math.floor(Math.random() * 3)+1;
+    cy.log(randomIndex)
+    cy.get('.list-group-item').eq(randomIndex).then($product => {
+        selectedGroupProduct = $product.text(); // Lưu tên hoặc thông tin của nhóm sản phẩm
+        cy.wrap($product).click();
+        cy.log(selectedGroupProduct)
+    });
+})
+Then('The product list is changed according to the selected product group',()=>{
+    //check after random click
+    if (selectedGroupProduct == 'Phones') cy.get('#tbodyid').find('.card-block').should('have.length', 7);
+    if (selectedGroupProduct == 'Laptops') cy.get('#tbodyid').find('.card-block').should('have.length', 6);
+    if (selectedGroupProduct == 'Monitors') cy.get('#tbodyid').find('.card-block').should('have.length', 2);
+})
+
+When('Click on CATEGORIES menu field',()=>{
+    cy.get('#cat').click()
+})
+Then('The list of product displays correctly with all products',()=>{
+    cy.get('#tbodyid').find('.card-block').should('have.length', 9)
+})
+
+//PRODUCT CARD
+Then('The card display information correctly',()=>{
+    cy.get('.card.h-100').eq(0).within(()=>{
+        cy.get('img').should('be.visible')
+        cy.get('.card-title').find('a').should('contain.text','Samsung galaxy s6')
+        cy.get('h5').should('contain.text','$360')
+        cy.get('#article').should('contain.text','The Samsung Galaxy S6 is powered by 1.5GHz')
+    })
+})
+
+When('Hover any title of product card',()=>{
+    cy.get('.card.h-100').eq(0).find('.card-title').find('a')
+    .trigger('mouseover');
+})
+
+Then('The title card is underlined',()=>{
+    cy.get('.card.h-100').eq(0).find('.card-title').find('a')
+    .should('have.css', 'text-decoration','none solid rgb(2, 117, 216)'); //chưa làm khúc check này
+})
+
+When('Click on any photo of product card',()=>{
+    cy.get('.card.h-100').eq(0).within(()=>{
+        cy.get('img').click()
+        cy.wait(2000)
+    })
+})
+Then('Redirect to an Information Product screen',()=>{
+    if (selectedGroupProduct == 'Phones') cy.url().should('include','https://demoblaze.com/prod.html?idp_=1');
+    if (selectedGroupProduct == 'Laptops') cy.url().should('include','https://demoblaze.com/prod.html?idp_=8');
+    if (selectedGroupProduct == 'Monitors') cy.url().should('include','https://demoblaze.com/prod.html?idp_=10');
+
+})
+When('Click on any title of product card',()=>{
+    cy.get('.card.h-100').eq(0).within(()=>{
+        cy.get('.card-title').find('a').click()
+        cy.wait(2000)
+    })
+})
+
+Then('The 2 buttons of pagination displays on the right and below list of cards',()=>{
+    cy.get('#frm').should('have.css', 'float', 'right');
+    cy.get('#next2').should('contain.text','Next')
+    cy.get('#prev2').should('contain.text','Previous')
+})
+
+When('Click on Next button',()=>{
+    cy.get('#next2').click()
+})
+Then('The next page displays with remaining product cards',()=>{
+    cy.get('#tbodyid').find('.card-block').should('have.length', 6);
+})
+
+When('Click on Previous button',()=>{
+    cy.get('#prev2').click()
+})
+Then('The previous page displays the list of first product cards',()=>{
+    cy.get('#tbodyid').find('.card-block').should('have.length', 9);
 })
